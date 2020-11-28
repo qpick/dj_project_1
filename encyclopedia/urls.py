174552b64f37692d05ea186fbe71fe06f116111d
Tasha <atanasovska87@hotmail.com>
2020-11-28 21:48:09 +0100

    Changes I made in Wikipedia project

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("create", views.create, name="create"),
+    path("wiki/<str:title>/edit", views.editEntry, name="editEntry"),
+    path("wiki/<str:title>/submit", views.submitEditEntry, name="submitEditEntry"),
+    path("wiki/", views.randomEntry, name="randomEntry")
 ]
