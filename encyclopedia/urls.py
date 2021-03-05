989c20437f38d7662cd29ec335f44b557dad6ce5
zoran <zoran_gogusev@yahoo.com>
2021-03-05 19:06:12 +0100

    Create encyclopedia urls, views, new pages new_entry and entry_detail, update index and layout pages

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("encyclopedia/<str:entry>", views.entry_detail, name="entry_detail"),
+    path("encyclopedia/new/", views.new_entry, name="new_entry"),
+    path("encyclopedia/edit/", views.edit_entry, name="edit_entry"),
+    path("encyclopedia/save/", views.save_entry, name="save_entry"),
+    path("encyclopedia/random/", views.random_entry, name="random_entry"),
+    path("encyclopedia/search/", views.search_entry, name="search_entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,44 @@
 from django.shortcuts import render
-
+from markdown2 import markdown
 from . import util
+from django import forms
+
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea())
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+    return render(request, 'encyclopedia/index.html', {
+        'entries': util.list_entries()
     })
 
+
+def entry_detail(request, entry):
+    return render(request, 'encyclopedia/entry_detail.html', {
+        'entry': markdown(util.get_entry(entry))
+    })
+
+
+def new_entry(request):
+    form = NewEntryForm()
+    return render(request, 'encyclopedia/new_entry.html', {
+        'form': form
+    })
+
+
+def save_entry(request):
+    pass
+
+
+def edit_entry(request):
+    pass
+
+
+def random_entry(request):
+    pass
+
+
+def search_entry(request):
+    pass
